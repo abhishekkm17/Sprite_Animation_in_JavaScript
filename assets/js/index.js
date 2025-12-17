5d60e02f5bcae3f1ec9be139ea603f99f7d7715f
Abhishek K M <abhishekkm1999s17@gmail.com>
2025-12-17 17:36:16 +0530

    Added animations, learning how to use frames.

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,17 +10,54 @@ const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = canvas.width=600;
 const CANVAS_HEIGHT = canvas.height=600;
 
+// The width and height of the sprite image  divided by the row and column count
+const spriteWidth=575;
+const spriteHeight=523;
+
 // Image() is a built-in constructor in JavaScript.
 // It creates a new HTMLImageElement (same as doing <img> in HTML).
 // This object behaves exactly like an <img> tag created dynamically with:
 const playerImage = new Image();
-playerImage.src='/assets/img/player.png';
+playerImage.src='/assets/images/player.png';
+
+
+
+let gameFrame=0;
+let frameX=0;
+let frameY=0;
+
+const staggerFrames=5;
+const spritAnimation=[];
+const animationStates=[{name:'idle',frame:7},{name:'jump',frame:7}];
+
+animationStates.forEach((state,index)=>{
+    
+    let frames={
+        loc:[],
+    }
+    for(let j=0;j<state.frame; j++){
+        
+        let positionX = j * spriteWidth;
+        let positionY = index *  spriteHeight;
+        frames.loc.push({x:positionX,y:positionY});
+    }
+    spritAnimation[state.name]=frames;
+});
+
+console.log(spritAnimation);
 
 function animate(){
     // Clear the entire canvas before each redraw
     // 0,0 is the coordinates of the top-left corner
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.fillRect(50,50,100,100); // Draw a filled rectangle at (50,50) with width 100 and height 100
+   
+    let position = Math.floor(gameFrame/staggerFrames)  % 6; // Assuming there are 6 frames in the sprite sheet
+    frameX = position * spriteWidth;
+   
+    // ctx.fillRect(100,50,100,100); // Draw a filled rectangle at (50,50) with width 100 and height 100
+    // ctx.drawImage(Image,sx,sy,sw,sh, dx, dy, dw, dh);
+    ctx.drawImage(playerImage,frameX,frameY * spriteHeight,spriteWidth,spriteHeight,0,0,spriteWidth,spriteHeight);
+    gameFrame++;
     requestAnimationFrame(animate); // Call animate again on the next frame
 }
 animate();
